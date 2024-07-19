712f65ec5c771d3656e4e1de20e09d88a530ef21
chibueze <chibuezeogezi@gmail.com>
2024-07-19 11:20:20 +0100

    First commit

---
+++ lib/first.dart
@@ -0,0 +1,10 @@
+void main() {
+//  print("hello world");
+
+  int age = 16;
+  print("my age is${age}");
+
+  // ignore: unused_local_variable
+  bool isFather = false;
+  // print("that is my father ${isFather}");
+}
